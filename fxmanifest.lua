b6b188f48e7be5708182c39e61c41846616d943f
gs4h97hfxg-ui <gs4h97hfxg@privaterelay.appleid.com>
2026-01-13 23:08:54 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'FH_Coding'
+description 'Fullscreen PNG Loadscreen mit Ladebalken'
+version '1.0.0'
+
+loadscreen 'index.html'
+
+files {
+    'index.html',
+    'style.css',
+    'background.png'
+}
